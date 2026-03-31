7328df75797fb98eee1578e0150afac517a910c4
Henry Zheng <henryzheng24@outlook.com>
2026-03-31 21:36:01 +0000

    fix bug

--- tests/test_equality.c
+++ tests/test_equality.c
@@ -8,9 +8,18 @@ int main(void)
         fprintf(stderr, "test_equality: the print statements in the functions under test are not silenced.\n");
     rand_init();
     for (ulong i = 0; i <= 32; i++)
-        for (ulong j = i; j <= 32; j++)
+        for (ulong j = 0; j <= 32; j++)
         {
             Parameters param = rand_parameters(i, j);
+            if (i > j && i <= 31 && j >= 2 && j <= 31 && param.p != 0)
+            {
+                FAIL("test_param", &err);
+                fprintf(stderr, "param should be {0}.\n");
+                ret = 1;
+                goto end;
+            }
+            else if (param.p == 0)
+                continue;
             Vector rand_v = rand_vector(VECTOR_SIZE);
 
             // Always correct for naive algorithm
