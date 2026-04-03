74f95dcac63859571eea52c22f24bdea2a77791a
Henry Zheng <henryzheng24@outlook.com>
2026-04-03 21:10:13 +0000

    Simplification

--- tests/test_options.c
+++ tests/test_options.c
@@ -21,6 +21,40 @@ static void uint32_to_str(uint32_t n, char buffer[])
     snprintf(buffer, BUFFER_SIZE - 1, "%u", n);
 }
 
+static int test_set_options(uint32_t p, uint32_t p_bits, uint32_t b, uint32_t b_bits, int *err)
+{
+    char p_str[BUFFER_SIZE], b_str[BUFFER_SIZE];
+    char p_bits_str[BUFFER_SIZE], b_bits_str[BUFFER_SIZE];
+    uint32_to_str(p, p_str);
+    uint32_to_str(b, b_str);
+    uint32_to_str(p_bits, p_bits_str);
+    uint32_to_str(b_bits, b_bits_str);
+    int verify_prime = (n_is_prime(p)) ? 0 : ERR_PRIME;
+    struct
+    {
+        int argc;
+        char const *argv[10];
+        char expected_flags;
+        int expected_ret;
+    } tests[] = {
+        {1, {"./pcca7"}, 0, 0},
+        {3, {"./pcca7", "-p", p_str}, OPT_P, verify_prime},
+        {3, {"./pcca7", "-p_bits", p_bits_str}, OPT_P_BITS, 0},
+        {3, {"./pcca7", "-b", b_str}, OPT_B, 0},
+        {3, {"./pcca7", "-b_bits", b_bits_str}, OPT_B_BITS, 0},
+        {5, {"./pcca7", "-p", p_str, "-b", b_str}, OPT_P | OPT_B, verify_prime},
+        {5, {"./pcca7", "-p_bits", p_bits_str, "-b", b_str}, OPT_P_BITS | OPT_B, 0},
+        {5, {"./pcca7", "-p", p_str, "-b_bits", b_bits_str}, OPT_P | OPT_B_BITS, verify_prime},
+        {5, {"./pcca7", "-p_bits", p_bits_str, "-b_bits", b_bits_str}, OPT_P_BITS | OPT_B_BITS, 0},
+        {7, {"./pcca7", "-p_bits", p_bits_str, "-p", p_str, "-b", b_str}, OPT_P_BITS | OPT_B, 0},
+        {7, {"./pcca7", "-p_bits", p_bits_str, "-not_exist", p_str, "-b", b_str}, OPT_P_BITS | OPT_B, ERR_UNRECOGNIZED},
+    };
+    for (size_t i = 0; i < TAB_SIZE(tests); i++)
+        if (test_options(tests[i].argc, tests[i].argv, tests[i].expected_flags, tests[i].expected_ret, err))
+            return 1;
+    return 0;
+}
+
 int main(void)
 {
     int out, err;
@@ -28,68 +62,16 @@ int main(void)
         fprintf(stderr, "test_options: the print statements in the functions under test are not silenced.\n");
     int ret = 0;
     for (int i = 0; i < NB_TESTS; i++)
-    {
-        char const *argv_1[] = {"./pcca7"};
-        ret = test_options(1, argv_1, 0, 0, &err);
-        if (ret == 1)
-            goto end;
-        for (ulong p_bits = 0; p_bits < 32; p_bits++)
+        for (uint32_t p_bits = 0; p_bits < 32; p_bits++)
         {
             uint32_t p = n_randbits(state, p_bits);
-            char p_str[BUFFER_SIZE];
-            uint32_to_str(p, p_str);
-            char const *argv_2[] = {"./pcca7", "-p", p_str};
-            ret = test_options(3, argv_2, OPT_P, (n_is_prime(p)) ? 0 : ERR_PRIME, &err);
-            if (ret == 1)
-                goto end;
-            char p_bits_str[BUFFER_SIZE];
-            uint32_to_str((uint32_t)p_bits, p_bits_str);
-            char const *argv_3[] = {"./pcca7", "-p_bits", p_bits_str};
-            ret = test_options(3, argv_3, OPT_P_BITS, 0, &err);
-            if (ret == 1)
-                goto end;
-            for (ulong b_bits = 0; b_bits < 32 && ret == 0; b_bits++)
+            for (uint32_t b_bits = 0; b_bits < 32; b_bits++)
             {
                 uint32_t b = n_randbits(state, b_bits);
-                char b_str[BUFFER_SIZE];
-                uint32_to_str(b, b_str);
-                char const *argv_4[] = {"./pcca7", "-b", b_str};
-                ret = test_options(3, argv_4, OPT_B, 0, &err);
-                if (ret == 1)
-                    goto end;
-                char b_bits_str[BUFFER_SIZE];
-                uint32_to_str((uint32_t)b_bits, b_bits_str);
-                char const *argv_5[] = {"./pcca7", "-b_bits", b_bits_str};
-                ret = test_options(3, argv_5, OPT_B_BITS, 0, &err);
-                if (ret == 1)
-                    goto end;
-                char const *argv_6[] = {"./pcca7", "-p", p_str, "-b", b_str};
-                ret = test_options(5, argv_6, OPT_P | OPT_B, (n_is_prime(p)) ? 0 : ERR_PRIME, &err);
-                if (ret == 1)
-                    goto end;
-                char const *argv_7[] = {"./pcca7", "-p_bits", p_bits_str, "-b", b_str};
-                ret = test_options(5, argv_7, OPT_P_BITS | OPT_B, 0, &err);
-                if (ret == 1)
-                    goto end;
-                char const *argv_8[] = {"./pcca7", "-p", p_str, "-b_bits", b_bits_str};
-                ret = test_options(5, argv_8, OPT_P | OPT_B_BITS, (n_is_prime(p)) ? 0 : ERR_PRIME, &err);
-                if (ret == 1)
-                    goto end;
-                char const *argv_9[] = {"./pcca7", "-p_bits", p_bits_str, "-b_bits", b_bits_str};
-                ret = test_options(5, argv_9, OPT_P_BITS | OPT_B_BITS, 0, &err);
-                if (ret == 1)
-                    goto end;
-                char const *argv_10[] = {"./pcca7", "-p_bits", p_bits_str, "-p", p_str, "-b", b_str};
-                ret = test_options(7, argv_10, OPT_P_BITS | OPT_B, 0, &err);
-                if (ret == 1)
-                    goto end;
-                char const *argv_11[] = {"./pcca7", "-p_bits", p_bits_str, "-not_exist", p_str, "-b", b_str};
-                ret = test_options(7, argv_11, OPT_P_BITS | OPT_B, ERR_UNRECOGNIZED, &err);
-                if (ret == 1)
+                if ((ret = test_set_options(p, p_bits, b, b_bits, &err)) == 1)
                     goto end;
             }
         }
-    }
     SUCCESS("test_options", &out);
 end:
     close(out);
